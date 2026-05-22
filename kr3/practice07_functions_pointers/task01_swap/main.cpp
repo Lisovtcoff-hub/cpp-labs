467636edc52d22b4c9eaca3767ae16a9ca5a675f
sergey <qwertyuiopa112358@gmail.com>
2026-05-23 01:53:06 +0500

    new_solutions: kr3/pr07/t1 & kr3/pr07/t2 architecture: added a folder for photo tasks

--- kr3/practice07_functions_pointers/task01_swap/main.cpp
+++ kr3/practice07_functions_pointers/task01_swap/main.cpp
@@ -2,10 +2,34 @@
 #include "console_utils.h"
 using namespace std;
 
+void swal(int* x, int* y)
+{
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
+}
+
 int main()
 {
     setupConsole();
 
-    return 0;
+    int x, y;
 
+    cout << "Введите x: ";
+    cin >> x;
+
+    cout << "Введите y: ";
+    cin >> y;
+
+    cout << "До обмена:" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    swal(&x, &y);
+
+    cout << "После обмена:" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
 }
--- kr3/practice07_functions_pointers/task02_function_pointer/main.cpp
+++ kr3/practice07_functions_pointers/task02_function_pointer/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <cmath>
 #include "console_utils.h"
 using namespace std;
+
+float f1(float x)
+{
+    return sin(x) / x;
+}
+
+float f2(float y)
+{
+    return y * y + 2 * y * sqrt(y);
+}
+
+float f3(float z)
+{
+    return (z + 2) * (z + 1) * z;
+}
+
+void print(float x, float (*y)(float)) // float *y(float) = y — функция, которая принимает float и возвращает указатель на float   А У НАС САМА у - УКАЗАТЕЛЬ НА ФУНК
+{
+    cout << y(x) << endl;
+}
 
 int main()
 {
     setupConsole();
 
-    return 0;
+    float x = 0.6;
+    float y = 0.3;
+    float z = 0.1;
 
+    cout << "f1(x) = ";
+    print(x, f1);
+
+    cout << "f2(y) = ";
+    print(y, f2);
+
+    cout << "f3(z) = ";
+    print(z, f3);
+
+    float R = f1(x) + f2(y) * f3(f2(z));
+
+    cout << "R(x, y, z) = " << R << endl;
+
+    return 0;
 }
